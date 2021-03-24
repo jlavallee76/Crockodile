a80d0cc051fb1c17615c351d2d41bd98b8f86fb4
Josh Lavallee <jlavallee76@academic.rrc.ca>
2021-03-24 14:08:55 -0500

    Added home page, created header, hero, featured section and footer

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   purge: [
-    './app/**/*.html.erb',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
+    // './app/**/*.html.erb',
+    // './app/helpers/**/*.rb',
+    // './app/javascript/**/*.js',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
