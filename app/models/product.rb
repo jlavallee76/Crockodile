f544553586b45ce0db4c39977a8c6cc240a8294d
Josh Lavallee <jlavallee76@academic.rrc.ca>
2021-03-26 09:05:05 -0500

    Ran rubocop for fixes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   validates :name, :price, :description, presence: true
   validates :name,
-    length: { maximum: 100, too_long: "%{count} charaters is the maximum allowed" }
+            length: { maximum: 100, too_long: "%{count} characters is the maximum allowed" }
   validates :description,
-    length: { maximum: 1000, too_long: "%{count} charaters is the maximum allowed" }
+            length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed" }
 end
